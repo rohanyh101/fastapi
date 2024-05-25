d22ba389fc5660e2c4058c80c440c18d6a85f69a
rohanyh101 <rohanyh101@gmail.com>
2024-05-26 00:54:54 +0530

    Next.js Hono Upstash Cloudflare Workers

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,77 @@
+import { Redis } from "@upstash/redis/cloudflare";
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { cors } from "hono/cors";
+// import { handle } from "hono/vercel";
+
+// this is just to deploy the api to the edge using vercel...
+// export const runtime = "edge";
+
+type EnvConfig = {
+	UPSTASH_REDIS_TOKEN: string;
+	UPSTASH_REDIS_URL: string;
+};
+
+const app = new Hono();
+
+app.use('/*', cors());
+
+app.get("/", (c) => {
+	return c.json({ message: "SpeedSearch API 🚀" });
+})
+
+app.get("/api/search", async (c) => {
+	try {
+		const { UPSTASH_REDIS_TOKEN, UPSTASH_REDIS_URL } = env<EnvConfig>(c);
+
+		// ------------
+		const start = performance.now();
+
+		const redis = new Redis({
+			url: UPSTASH_REDIS_URL,
+			token: UPSTASH_REDIS_TOKEN,
+		});
+
+		const query = c.req.query("q")?.toUpperCase();
+
+		if (!query) {
+			return c.json({ message: "invalid search query..." }, { status: 400 });
+		}
+
+		const res = [];
+		const rank = await redis.zrank("terms", query);
+
+		if (rank !== null && rank !== undefined) {
+			const temp = await redis.zrange<string[]>("terms", rank, rank + 50);
+
+			for (const e of temp) {
+				if (!e.startsWith(query)) {
+					break;
+				}
+
+				if (e.endsWith("*")) {
+					res.push(e.substring(0, e.length - 1));
+				}
+			}
+		}
+
+		// -------------
+		const end = performance.now();
+
+		return c.json({
+			results: res,
+			duration: end - start,
+		});
+	} catch (err) {
+		console.log(err);
+
+		return c.json(
+			{ results: [], message: "something went wrong..." },
+			{ status: 500 },
+		);
+	}
+});
+
+// again below is for deploying the api to the edge using vercel...
+// export const GET = handle(app);
+export default app;
---
+++ src/app/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "@/components/ui/command";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+	const [input, setInput] = useState<string>("");
+	const [searchResults, setSearchResults] = useState<{
+		results: string[];
+		duration: number;
+	}>();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!input) return setSearchResults(undefined);
+
+			const res = await fetch(`https://fastapi.tempoonlyacc.workers.dev/api/search?q=${input}`);
+			const data = (await res.json()) as {
+				results: string[];
+				duration: number;
+			};
+			setSearchResults(data);
+		};
+
+		fetchData();
+	}, [input]);
+
+	return (
+		<main className="h-screen w-screen grainy">
+			<div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+				<h1 className="text-5xl tracking-tight font-bold ">SpeedSearch ⚡ </h1>
+				<p className="text-zinc-600 text-lg max-w-prose text-center">
+					A high-performance API built with Hono, Next.js, Cloudflare.
+					<br /> Type Something and get results in milliseconds...
+				</p>
+
+				<div className="max-w-md w-full">
+					<Command>
+						<CommandInput
+							value={input}
+							onValueChange={setInput}
+							placeholder="search countries...."
+							className="placeholder: text-zinc-500"
+						/>
+						<CommandList>
+							{searchResults?.results.length === 0 ? (
+								<CommandEmpty>No results found</CommandEmpty>
+							) : null}
+
+							{searchResults?.results ? (
+								<CommandGroup heading="Results">
+									{searchResults.results.map((result) => (
+										<CommandItem
+											key={result}
+											value={result}
+											onSelect={setInput}
+										>
+											{result}
+										</CommandItem>
+									))}
+								</CommandGroup>
+							) : null}
+
+							{searchResults?.results ? (
+								<>
+									<div className="h-px w-full bg-zinc-200" />
+
+									<p className="p-2 text-xs text-zinc-500">
+										Found {searchResults.results.length} results in{" "}
+										{searchResults.duration.toFixed(0)}ms
+									</p>
+								</>
+							) : null}
+						</CommandList>
+					</Command>
+				</div>
+
+				{/* <input
+					className="text-zinc-500"
+					value={input}
+					type="text"
+					onChange={(e) => setInput(e.target.value)}
+				/> */}
+			</div>
+		</main>
+	);
+}
